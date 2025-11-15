7b5da58b70c0ab782fd6063350ccced800401751
VB2007 <admin@vb2007.hu>
2025-11-15 23:43:30 +0100

    added supported categories

--- milkie.py
+++ milkie.py
@@ -4,7 +4,14 @@ class milkie(object):
     url = "https://milkie.cc/api/v1/torrents"
     name = "Milkie"
     supported_categories = {
-
+        "all": "0",
+        "movies": "1",
+        "tv": "2",
+        "music": "3",
+        "games": "4",
+        "ebooks": "5",
+        "apps": "6",
+        "adult": "7"
     }
     # get key from: https://milkie.cc/settings/security
     api_key = "your_key_goes_here"
